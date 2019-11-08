c0161a324c762a66418e1db869da6103a89cfeff
Naty-S <na7a5cha010@gmail.com>
2019-11-08 10:01:48 -0400

    Fixes

--- test/Lexer/OperatorsSpec.hs
+++ test/Lexer/OperatorsSpec.hs
@@ -8,21 +8,19 @@ spec = describe "Lexer.operators" $ do
   
   describe "numeric" $ do
     it "accepts `5 + 3 * 10` as a valid plus and mult operators" $ do
-        let ops = "+*"
-            tokens = alexScanTokens "5 + 3 * 10"
+        let tokens = alexScanTokens "5 + 3 * 10"
 
         case tokens of
           [TkINT _ _ _,TkADD op1 _,TkINT _ _ _,TkMULT op2 _,TkINT _ _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` "+*"
           _ -> error "rejected as invalid operators"
 
     it "accepts `7 - 2 / 5` as a valid minus and div operators" $ do
-        let ops = "-/"
-            tokens = alexScanTokens "7 - 2 / 5"
+        let tokens = alexScanTokens "7 - 2 / 5"
 
         case tokens of
           [TkINT _ _ _,TkMIN op1 _,TkINT _ _ _,TkDIV op2 _,TkINT _ _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` "-/"
           _ -> error "rejected as invalid operators"
 
     it "accepts `//` as a valid integer div operator" $ do
@@ -62,39 +60,35 @@ spec = describe "Lexer.operators" $ do
 
   describe "comparison" $ do
     it "accepts `a || b && c` as a valid disjuction and conjuction operators" $ do
-        let ops = "||&&"
-            tokens = alexScanTokens "a || b && c"
+        let tokens = alexScanTokens "a || b && c"
 
         case tokens of
           [TkID _ _,TkOR op1 _,TkID _ _,TkAND op2 _,TkID _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` "||&&"
           _ -> error "rejected as an invalid operator"
 
     it "accepts `a <= b == c` as a valid less equal and equal operators" $ do
-        let ops = "<==="
-            tokens = alexScanTokens "a <= b == c"
+        let tokens = alexScanTokens "a <= b == c"
 
         case tokens of
           [TkID _ _,TkLessEqual op1 _,TkID _ _,TkEQUAL op2 _,TkID _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` "<==="
           _ -> error "rejected as invalid operators"
 
     it "accepts `a >= b < c` as a valid greater equal and less than operators" $ do
-        let ops = ">=<"
-            tokens = alexScanTokens "a >= b < c"
+        let tokens = alexScanTokens "a >= b < c"
 
         case tokens of
           [TkID _ _,TkGreaterEqual op1 _,TkID _ _,TkLessThan op2 _,TkID _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` ">=<"
           _ -> error "rejected as invalid operators"
 
     it "accepts `a > b != c` as a valid greater than and not equal operators" $ do
-        let ops = ">!="
-            tokens = alexScanTokens "a > b != c"
+        let tokens = alexScanTokens "a > b != c"
 
         case tokens of
           [TkID _ _,TkGreaterThan op1 _,TkID _ _,TkNotEqual op2 _,TkID _ _] ->
-            op1 ++ op2 `shouldBe` ops
+            op1 ++ op2 `shouldBe` ">!="
           _ -> error "rejected as invalid operators"
 
     it "accepts `!` as a valid not operator" $ do
