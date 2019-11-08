3440863426ed4d1de925577c5e1790ceb501fce9
Naty-S <na7a5cha010@gmail.com>
2019-11-08 07:06:50 -0400

    Fixes of pattern matches

--- test/Lexer/LiteralsSpec.hs
+++ test/Lexer/LiteralsSpec.hs
@@ -1,4 +1,4 @@
-module CharLiteralsSpec where
+module LiteralsSpec where
 
 import Test.Hspec
 import Playit.Lexer
@@ -11,65 +11,65 @@ spec = describe "Lexer.literals" $ do
       tokens <- alexScanTokens "*a*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*a*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*a*"
+        _ -> error "rejected as a char literal"
       
     it "accepts `*b*` as a valid char literal" $ do
       tokens <- alexScanTokens "*b*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*b*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*b*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\n*` as a valid char literal" $ do
       tokens <- alexScanTokens "*\n*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\n*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\n*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\0*` as a valid char literal" $ do
       tokens <- alexScanTokens "*\0*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\0*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\0*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\\*` as a valid char literal" $ do
       tokens <- alexScanTokens "*\\*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\\*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\\*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\t*` as a valid char literal" $ do
       tokens <- alexScanTokens "*\t*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\t*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\t*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\\~*` as a valid char literal" $ do
       tokens <- alexScanTokens "*\\~*"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\\~*"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\\~*"
+        _ -> error "rejected as a char literal"
 
     it "accepts `*\**` as a valid char literal" $ do
       tokens <- alexScanTokens "*\**"
 
       case tokens of
-        [TkRUNE char _ _] -> char `shouldBe` "*\**"
-        _ -> error "rejected as an invalid token"
+        [TkCHARACTER char _ _] -> char `shouldBe` "*\**"
+        _ -> error "rejected as a char literal"
 
   decribe "strings" $ do
     it "accepts `~Un str1ng b!3n c0mplet@ !\"#$%&/()=?\'*+<>^{}[]`_.,;.~` as a valid string literal" $ do
       tokens <- alexScanTokens "accepts `~Un str1ng b!3n c0mplet@ !\"#$%&/()=?\'*+<>^{}[]`_.,;.~"
 
       case tokens of
-        [TkRUNES str _] -> str `shouldBe` "accepts `~Un str1ng b!3n c0mple7@ \"#$%&/()=?\'*+<>^{}[]`_.,;.~"
-        _ -> error "rejected as an invalid token"
+        [TkSTRINGS str _] -> str `shouldBe` "accepts `~Un str1ng b!3n c0mple7@ \"#$%&/()=?\'*+<>^{}[]`_.,;.~"
+        _ -> error "rejected as a string literal"
 
     it "rejects `~*~\\~` as a valid string literal" $ do
       tokens <- alexScanTokens "~*~\\~"
@@ -84,28 +84,28 @@ spec = describe "Lexer.literals" $ do
 
       case tokens of
         [TkINT num _ _] -> num `shouldBe` "0000004"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as an integer literal"
   
     it "accepts `09029043856234700` as a valid integer literal" $ do
       tokens <- alexScanTokens "09029043856234700"
 
       case tokens of
         [TkINT num _ _] -> num `shouldBe` "09029043856234700"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as an integer literal"
   
     it "accepts `8734'8346` as a valid float literal" $ do
       tokens <- alexScanTokens "8734'8346"
 
       case tokens of
         [TkFLOAT num _ _] -> num `shouldBe` "8734'8346"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as a float literal"
   
     it "accepts `0192823'0987` as a valid float literal" $ do
       tokens <- alexScanTokens "0192823'0987"
 
       case tokens of
         [TkFLOAT num _ _] -> num `shouldBe` "0192823'0987"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as a float literal"
   
   decribe "booleans" $ do
     it "accepts `Win` as a valid boolean literal" $ do
@@ -113,12 +113,12 @@ spec = describe "Lexer.literals" $ do
 
       case tokens of
         [TkWIN bool _] -> bool `shouldBe` "Win"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as a boolean literal"
 
     it "accepts `Lose` as a valid boolean literal" $ do
       tokens <- alexScanTokens "Lose"
 
       case tokens of
         [TkLOSE bool _] -> bool `shouldBe` "Lose"
-        _ -> error "rejected as an invalid token"
+        _ -> error "rejected as a boolean literal"
 
