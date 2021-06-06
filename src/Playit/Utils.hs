40bb89ca5e9ff551aef5833e0b40ae9c4144da87
Naty-S <na7a5cha010@gmail.com>
2021-06-06 02:26:58 -0400

    Module for common used strings

---
+++ src/Playit/Utils.hs
@@ -0,0 +1,79 @@
+{- |
+ *  Escape sequences for colors and common used symbols
+ *
+ * Copyright : (c) 
+ *  Manuel Gonzalez     11-10390
+ *  Francisco Javier    12-11163
+ *  Natascha Gamboa     12-11250
+-}
+module Playit.Utils where
+
+import qualified Data.ByteString.Lazy       as BL
+import qualified Data.ByteString.Lazy.Char8 as BLC
+-- import           Data.List (intercalate)
+
+-- joinWithCommas :: (Show a) => [a] -> String
+-- joinWithCommas = intercalate ", " . map show
+
+newLine :: BL.ByteString
+newLine = BLC.pack "\n"
+
+tab :: BL.ByteString
+tab = BLC.pack "\t"
+
+pipeline :: BL.ByteString
+pipeline = BLC.pack "| "
+
+twoDots :: BL.ByteString
+twoDots = BLC.pack ": "
+
+nocolor :: BL.ByteString
+nocolor = BLC.pack "\x1b[0m"
+
+italic :: BL.ByteString
+italic = BLC.pack "\x1b[3m"
+
+bold :: BL.ByteString
+bold = BLC.pack "\x1b[1m"
+
+underline :: BL.ByteString
+underline = BLC.pack "\x1b[4m"
+
+tilt :: BL.ByteString
+tilt = BLC.pack "\x1b[5;31m"
+
+-- dim :: BL.ByteString
+-- dim = BLC.pack "\x1b[2m"
+
+blue :: BL.ByteString
+blue = BLC.pack "\x1b[94m"
+
+blue' :: BL.ByteString
+blue' = BLC.pack "\x1b[1;94m"
+
+yellow :: BL.ByteString
+yellow = BLC.pack "\x1b[93m"
+
+yellow' :: BL.ByteString
+yellow' = BLC.pack "\x1b[1;93m"
+
+red :: BL.ByteString
+red = BLC.pack "\x1b[1;91m"
+
+cyan :: BL.ByteString
+cyan = BLC.pack "\x1b[1;36m"
+
+cyan' :: BL.ByteString
+cyan' = BLC.pack "\x1b[0;96m"
+
+-- magenta :: BL.ByteString
+-- magenta = BLC.pack "\x1b[35m"
+
+-- brightMagenta :: BL.ByteString
+-- brightMagenta = BLC.pack "\x1b[95m"
+
+-- brightRed :: BL.ByteString
+-- brightRed = BLC.pack "\x1b[91m"
+
+-- green :: BL.ByteString
+-- green = BLC.pack "\x1b[32m"
